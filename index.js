2caf21d1a09eaf8ed481bc752a3629cdf3aa6491
Charles Guerrero <charles.allfiction@gmail.com>
2021-09-01 21:15:46 +0800

    Act 2 done

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('nodebody-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
 
+//routes
 const employee = require('./routes/employee');
 app.use('/employee',employee);
 
-mongoose.connect('mongodb://localhost:27017/mernstack', {
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'client','build','index.html'));
+    });
+}
+
+const uri = process.env.mongodb || 'mongodb://localhost:27017/mernstack';
+mongoose.connect(uri,
+{
     useNewUrlParser: true,
-    useFindAndModify: false,
-},(err)=> {
+    useFindAndModify: false
+},(err)=>{
     if(err){
         process.exit(1);
-        console.log('unable to connect database');
+        console.log('unable to connect to database');
     }
     else
-        console.log('database connected successfully');
+        console.log('successfully connected to the database');
 });
 
 const port = process.env.PORT || 5000;
